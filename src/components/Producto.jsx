e5d2fcc691752c73e1840d7345b41975785a06ac
Abraham Cardenas <programmer996633@gmail.com>
2024-01-22 13:19:28 -0400

    agregar items al carrito

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,4 +1,17 @@
 export default function Producto({ product }) {
+  function addToCart(id) {
+    let cart = {};
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+    if (cart[id]) {
+      cart[id]++;
+    } else {
+      cart[id] = 1;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
   return (
     <div className="product-card" key={product.id}>
       <img src={product.image} alt={product.title} />
@@ -8,8 +21,7 @@ export default function Producto({ product }) {
         <p className="description">{product.description}</p>
         <button
           onClick={() => {
-            // TODO: agregar funcionalidad aca
-            // addToCart(product.id);
+            addToCart(product.id);
           }}
         >
           Agregar al carrito
