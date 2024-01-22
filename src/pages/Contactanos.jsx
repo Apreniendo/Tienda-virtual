67ce843e1baad7045e0266fdb761046563e72f81
Abraham Cardenas <programmer996633@gmail.com>
2024-01-22 12:30:56 -0400

    agregar pagina web de contactanos

--- src/pages/Contactanos.jsx
+++ src/pages/Contactanos.jsx
@@ -1,3 +1,26 @@
 export default function Contactanos() {
-  return <>Contactanos</>;
+  return (
+    <>
+      <h2>Contactanos</h2>
+      <form id="contact-form">
+        <div>
+          <label for="name">Nombre:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label for="email">Correo Electrónico:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label for="asunto">Asunto:</label>
+          <input type="text" id="asunto" name="asunto" required />
+        </div>
+        <div>
+          <label for="message">Mensaje:</label>
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
 }
