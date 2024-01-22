170f7ad4601ae91419030fdb4be66f8f72d2efa5
Abraham Cardenas <programmer996633@gmail.com>
2024-01-22 12:23:12 -0400

    agregar contenido de la pagina de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <>Pagina principal</>;
+  return (
+    <>
+      <h2>Bienvenido a Tu Tienda de Electrónica de Juegos</h2>
+      <p>
+        En nuestra tienda encontrarás todo lo que necesitas para disfrutar al
+        máximo de tus juegos favoritos. Ofrecemos una amplia selección de
+        consolas, videojuegos, accesorios y más.
+      </p>
+      <p>
+        Además, contamos con un equipo de expertos en videojuegos que están
+        siempre dispuestos a ayudarte a encontrar lo que necesitas y a responder
+        cualquier pregunta que tengas.
+      </p>
+      <p>¡Visítanos hoy mismo y descubre todo lo que tenemos para ofrecerte!</p>
+    </>
+  );
 }
